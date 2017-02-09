f42436594071bed97f5a31a9b2d5b94d443574fe
Sam Messina <samuel.messina@gmail.com>
2017-02-09 08:50:31 -0600

    Connection with SQL is working - no queries yet

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express     = require('express');
 const app         = express();
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser')
+const mysql       = require('mysql');
 
 require('dotenv').config();
 
@@ -19,70 +19,41 @@ if (process.env.NODE_ENV === 'production') {
 
 let db_user = process.env.DB_USER;
 let db_pw   = process.env.DB_PASSWORD;
-let db_url  = 'mongodb://'          +
-              db_user + ':' + db_pw +
-              '@ds139989.mlab.com:39989/regexpressyourself-todo';
 let db;
 
-MongoClient.connect(db_url, (err, database) => {
-    if (err) {
-        return console.log(err);
-    }
-    db = database;
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : db_user,
+  password : db_pw,
+  database : 'todo'
 });
 
+connection.connect();
+
+/*
+   connection.query("INSERT INTO users (name) VALUES ('Billy')");
+ */
+
 app.get('/projects', (req, res) => {
-    let collectionName = "sam"; //update which user
-    db.collection(collectionName).find().toArray(function(err, results) {
-        let projectList = [];
-        if (results) {
-            for (let i = 0; i < results.length; i++) {
-                let projectId = results[i]["_id"];
-                let projectName = results[i]["project-name"];
-                projectList.push(
-                    {projectId: projectId,
-                     projectName: projectName
-                    }
-                );
-            }
-        }
-        res.send(projectList);
-    });
+})
+
+app.get('/stages', (req, res) => {
+})
+
+app.get('/issues', (req, res) => {
 })
 
 app.post('/new-project', (req, res) => {
-    let collectionName = "sam"; //update which user
-    db.collection(collectionName).save(req.body, (err, result) => {
-        if (err) {
-            return console.log("oops: " + err);
-        }
-        console.log('saved to database')
-        res.redirect('/')
-    })
 })
 
 app.post('/new-stage', (req, res) => {
-    let collectionName = "sam"; //update which user
-    db.collection(collectionName).save(req.body, (err, result) => {
-        if (err) {
-            return console.log("oops: " + err);
-        }
-        console.log('saved to database')
-        res.redirect('/')
-    })
 })
 
 app.post('/new-issue', (req, res) => {
-    let collectionName = "sam"; //update which user
-    db.collection(collectionName).save(req.body, (err, result) => {
-        if (err) {
-            return console.log("oops: " + err);
-        }
-        console.log('saved to database')
-        res.redirect('/')
-    })
 })
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
+
+connection.end();
