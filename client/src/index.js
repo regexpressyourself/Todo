beb8d008c031b36725ff56fd3f86079387ad112a
Sam Messina <samuel.messina@gmail.com>
2017-02-10 12:33:56 -0600

    Started react routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-/* import routes from './config/routes';*/
-import App from './components/App';
+import routes from './config/routes';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    routes,
+    document.getElementById('root')
 );
