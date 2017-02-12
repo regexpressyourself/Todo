1a20e0f6e5ca60e28a1209e11e7ad08d6417bb93
Sam Messina <samuel.messina@gmail.com>
2017-02-12 14:11:43 -0600

    Added sass support to webpack

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -115,6 +115,11 @@ module.exports = {
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
       {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: ["style", "css", "sass"]
+      },
+      {
         test: /\.css$/,
         loader: 'style!css?importLoaders=1!postcss'
       },
