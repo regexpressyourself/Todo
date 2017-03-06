a52377f67a03f5c1ad8e03101e396cde9f9e3d63
Sam Messina <samuel.messina@gmail.com>
2017-03-05 22:06:02 -0600

    Move routes to a seperate file

---
+++ config/routes.js
@@ -0,0 +1,101 @@
+module.exports = function(app, passport) {
+    const db             = require('./db');
+    const {add_project_by_id,
+           get_projects_by_email,
+           get_projects_by_userid,
+           get_project_by_id,
+           add_new_stage_to_project} = db;
+
+
+    app.post('/create-account',
+             // Create account form submits here
+             passport.authenticate('local-signup',
+                                   { successRedirect: '/projects',
+                                     failureRedirect: '/',
+                                     failureFlash: true}));
+
+    app.post('/login', 
+             passport.authenticate('local-login',
+                                   { successRedirect: '/projects',
+                                     failureRedirect: '/',
+                                     failureFlash: true}));
+
+
+    app.get('/api/isLoggedIn', (req, res) => {
+        // Check if user is logged in
+        let isLoggedIn = false;
+        if (req.isAuthenticated()) {
+            // Send true if user is logged in
+            isLoggedIn = true;
+            res.send(isLoggedIn);
+        }
+        else {
+            // Send false if user is not logged in
+            res.send(isLoggedIn);
+        }
+        // if they aren't redirect them to the home page
+    });
+
+    app.get('/api/project', (req, res) => {
+        // Get a project by id
+        if (req.query.projectId){
+            get_project_by_id(req.query.projectId, (project) => {
+                res.send(project[0]);
+            });
+        }
+    });
+
+    app.get('/api/projects', (req, res) => {
+        // Get projects by userId
+        if (req.user && req.user.id && req.body) {
+            get_projects_by_userid(req.user.id, (project_list) => {
+                res.send(project_list);
+            });
+        }
+    });
+
+    app.get('/api/stages', (req, res) => {
+    });
+
+    app.get('/api/issues', (req, res) => {
+    });
+
+    app.post('/api/new-project', (req, res) => {
+        if (req.body &&
+            req.body['projectName'] &&
+            req.user &&
+            req.user.id) {
+            add_project_by_id(req.body.projectName, req.body.stageList, req.user.id);
+            res.redirect('/projects');
+        }
+        else {
+            res.redirect('/');
+        }
+    });
+
+    app.post('/api/new-stage', (req, res) => {
+        if (req.body &&
+            req.user &&
+            req.user.id &&
+            req.body['stage-name'] &&
+            req.body['stage-order'] &&
+            req.body['parent-project']) {
+
+            add_new_stage_to_project(req.body['stage-name'],
+                                     req.body['stage-order'],
+                                     req.body['parent-project']);
+            res.redirect('/projects/views/kanban/' + req.body['parent-project']);
+        }
+    });
+
+    app.post('/api/new-issue', (req, res) => {
+    });
+
+    app.post('/api/update-stage-order', (req, res) => {
+        // TODO make this work
+    });
+
+    app.post('/api/edit-project', (req, res) => {
+    });
+
+}
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
+require('dotenv').config();
 const express        = require('express');
 const session        = require('express-session');
 const app            = express();
 const bodyParser     = require('body-parser');
 const cookieParser   = require('cookie-parser');
-const db             = require('./config/db');
 const passport       = require('passport');
 const flash          = require('connect-flash');
 const session_secret = process.env.SESSION_SECRET;
-
-const {add_project_by_id,
-       get_projects_by_email,
-       get_projects_by_userid,
-       get_project_by_id,
-       add_new_stage_to_project} = db;
 
 require('./config/passport')(passport); // pass passport for configuration
 
@@ -43,94 +37,7 @@ if (process.env.NODE_ENV === 'production') {
 const USEREMAIL = 'samuel.messina@gmail.com'; // TODO get user data from login
 //const USERID    = 1; // TODO get user id from login
 
-app.post('/create-account',
-         // Create account form submits here
-         passport.authenticate('local-signup',
-                               { successRedirect: '/projects',
-                                 failureRedirect: '/',
-                                 failureFlash: true}));
-
-app.post('/login', 
-         passport.authenticate('local-login',
-                               { successRedirect: '/projects',
-                                 failureRedirect: '/',
-                                 failureFlash: true}));
-
-
-app.get('/api/isLoggedIn', (req, res) => {
-    // Check if user is logged in
-    let isLoggedIn = false;
-    if (req.isAuthenticated()) {
-        // Send true if user is logged in
-        isLoggedIn = true;
-        res.send(isLoggedIn);
-    }
-    else {
-        // Send false if user is not logged in
-        res.send(isLoggedIn);
-    }
-    // if they aren't redirect them to the home page
-});
-
-app.get('/api/project', (req, res) => {
-    // Get a project by id
-    if (req.query.projectId){
-        get_project_by_id(req.query.projectId, (project) => {
-            res.send(project[0]);
-        });
-    }
-});
-
-app.get('/api/projects', (req, res) => {
-    // Get projects by userId
-    if (req.user && req.user.id && req.body) {
-        get_projects_by_userid(req.user.id, (project_list) => {
-            res.send(project_list);
-        });
-    }
-});
-
-app.get('/api/stages', (req, res) => {
-});
-
-app.get('/api/issues', (req, res) => {
-});
-
-app.post('/api/new-project', (req, res) => {
-    if (req.body &&
-        req.body['projectName'] &&
-        req.user &&
-        req.user.id) {
-        add_project_by_id(req.body.projectName, req.body.stageList, req.user.id);
-        res.redirect('/projects');
-    }
-    else {
-        res.redirect('/');
-    }
-});
-
-app.post('/api/new-stage', (req, res) => {
-    if (req.body &&
-        req.body['stage-name'] &&
-        req.body['stage-order'] &&
-        req.body['parent-project']) {
-
-        add_new_stage_to_project(req.body['stage-name'],
-                                 req.body['stage-order'],
-                                 req.body['parent-project']);
-        res.redirect('/projects/views/kanban/' + req.body['parent-project']);
-    }
-});
-
-app.post('/api/new-issue', (req, res) => {
-});
-
-app.post('/api/edit-project', (req, res) => {
-    if (req.body && req.body['project-name']) {
-        add_project_by_email(req.body['project-name'], USEREMAIL);
-        res.redirect('/');
-    }
-});
+require('./config/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 app.listen(app.get('port'), () => {
     console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
