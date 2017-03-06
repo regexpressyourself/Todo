d62490cae171ac7e1bcd53abc5d254a756bb475d
Sam Messina <samuel.messina@gmail.com>
2017-03-06 09:21:04 -0600

    Hard coded new user and login forms

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,12 +9,34 @@ class Home extends React.Component {
                 <h1>
                     Wow!
                 </h1>
-                <NewProject></NewProject>
-                <br/>
                 <hr/>
-                <br/>
-                <NewIssue></NewIssue>
+                <h2>Create</h2>
+                <form method="post" action="/create-account">
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input name="email" type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input name="password" className="form-control" type="password" />
+                    </div>
+  <button type="submit" className="btn btn-default">Submit</button>
 
+                </form>
+                <br/>
+                <h2>Login</h2>
+                <form method="post" action="/login">
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input name="email" type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input name="password" className="form-control" type="password" />
+                    </div>
+  <button type="submit" className="btn btn-default">Submit</button>
+
+                </form>
             </div>
         )
     }
