15a25c1a8d9ed71b5c5841fe5dd86eb5ffe1e377
Sam Messina <samuel.messina@gmail.com>
2017-03-21 16:24:15 -0500

    Add beginning of kanban issue component

---
+++ client/src/components/KanbanIssue.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class KanbanIssue extends React.Component {
+    render() {
+        return (
+            <div className="kanban-issue">
+                hello
+            </div>
+        )
+    }
+}
+
+export default KanbanIssue;
